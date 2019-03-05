2ff7192eca6de6928db0383f68501a6385f17baa
elyar <elyar.ld@gmail.com>
2019-03-05 00:49:03 -0600

    First app version

---
+++ manualBPM/service-worker.js
@@ -0,0 +1,35 @@
+var dataCacheName = 'BPMeter';
+var cacheName = 'BPMeter';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/logic.js',
+  '/style.css',
+  '/imgs/metronome.png',
+  '/imgs/metronome2.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
